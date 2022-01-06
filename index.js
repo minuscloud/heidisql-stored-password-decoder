3423decb91ea15d252a456f01700d402e6b71621
Rubin <68704042+0xRubin@users.noreply.github.com>
2022-01-06 22:21:14 +0100

    check if textbox is empty

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ function heidiDecoder(hex) {
     var str = '';
     var shift = parseInt(hex.substr(-1));
     hex = hex.substr(0, hex.length - 1);
+
+    if (document.getElementById('input').value == "" ){
+        alert("Please enter a password")
+        return;
+    }
     for (var i = 0; i < hex.length; i += 2) str += String.fromCharCode(parseInt(hex.substr(i, 2), 16) - shift);
     document.getElementById("output").innerHTML = "Decoded password: " + str;
 }
